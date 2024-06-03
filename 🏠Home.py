ab6957b7a7a5d2eab62b2a9cb94fce282342e365
MurithiDenisGitobu <iamdenis46@gmail.com>
2024-06-03 21:43:24 +0300

    Our first commit to the setup branch

---
+++ 🏠Home.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+# Set up Home page
+st.set_page_config(
+    page_title="CUSTOMER CHURN PREDICTION APPLICATION",
+    page_icon='🏠',
+    layout="wide"
+)
+
+# Title and introduction
+st.title("CUSTOMER CHURN PREDICTION APPLICATION")
+st.subheader("🔮 Predict the Future! 🔮")
+# st.image("Assets\AI_Generated_Image_2024-06-03_455117497009201.png")
+st.write(""" 
+This application harnesses the power of data science to forecast if a customer 
+is about to churn based on known characteristics. Our solution leverages data science 
+to empower organizations with actionable insights, enabling proactive retention strategies
+and sustainable growth.
+
+**Key Features:**
+- Accurate Prediction: Utilize predictive modeling to anticipate customer churn with precision.
+- Data-Driven Decisions: Leverage comprehensive customer data to inform strategic initiatives.
+- Proactive Retention: Take preemptive measures to retain valuable customers and foster long-term loyalty.
+""")
